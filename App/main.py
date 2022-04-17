999d100f32f092c7a9485b51d01780f89a33c506
Abhinav <abhinava2560@gmail.com>
2022-04-17 05:58:21 +0000

    I have stomach pain

--- App/main.py
+++ App/main.py
@@ -1,6 +1,40 @@
 import sys
 from predict import Predictor
-text = " ".join(sys.argv[1:])
+
+import speech_recognition as sr
+import pyaudio
+import pandas as pd
+
+import pyaudio
+import wave
+import sys
+import time
+
+from gtts import gTTS
+from playsound import playsound
+import os, time
+
+done = False
+print(time.perf_counter())
+r = sr.Recognizer()
+print(time.perf_counter())
+while done == False:
+    with sr.Microphone() as source:
+        print(time.perf_counter())
+        print("Recording")
+        # read the audio data from the default microphone
+        audio_data = r.record(source, duration=5)
+        print("Recognizing...")
+        # convert speech to text
+        try:
+            text = r.recognize_google(audio_data)
+            done = True
+        except:
+            pass
+        # text = " ".join(sys.argv[1:])
+        # print(text)
+
+#######
 
 predictor = Predictor()
 output = predictor.filterText(text)
@@ -21,3 +55,52 @@ for o in output:
 
 with open("diagnosis.txt", "a") as f:
     f.write(result)
+
+symptomDescription = pd.read_csv('Data/symptom_Description.csv')
+symptomPrecaution = pd.read_csv('Data/symptom_precaution.csv')
+
+with open("diagnosis.txt","a") as f:
+    for o in output:
+        f.write("\n If you think you have " + o[0] + "then we reccomend that you take the following precaution \n")
+        f.write((symptomPrecaution.loc[symptomPrecaution['Disease']==o[0]]).iat[0,1])
+        f.write("\n What is " + o[0] + "exactly, you may ask. Well, I'd be glad to tell you! \n")
+        f.write((symptomDescription.loc[symptomDescription['Disease']==o[0]]).iat[0,1])
+        
+
+with open('diagnosis.txt', 'r') as f:
+    inpText = f.read().replace('\n', ' ')
+
+
+
+
+#########
+
+CHUNK = 1024
+
+PyAudio = pyaudio
+language = 'en'
+
+
+inputText = inpText
+speechObj = gTTS(text=inputText,lang=language,slow=False)
+speechObj.save("outputVoice.wav")
+playsound('outputVoice.wav')
+wavFile = wave.open("outputVoice.wav",'rb')
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=p.get_format_from_width(wavFile.getsampwidth()),
+                    channels=wavFile.getnchannels(),
+                    rate=wavFile.getframerate(),
+                    output=True)
+
+data = wavFile.readframes(CHUNK)
+
+while data != '':
+        stream.write(data)
+        data = wavFile.readframes(CHUNK)
+
+stream.stop_stream()
+stream.close()
+
+p.terminate()
