c991bc8c738ab60c404400e0d583099a4599a0b4
Alan-JH <alanjhsu08@gmail.com>
2022-04-17 04:13:38 -0400

    revert

--- App/main.py
+++ App/main.py
@@ -1,6 +1,41 @@
 import sys
 from predict import Predictor
-text = " ".join(sys.argv[1:])
+from data import SEVERITY, SEVERE_THRESHOLD
+
+import speech_recognition as sr
+import pyaudio
+import pandas as pd
+
+import pyaudio
+import wave
+import sys
+import time
+
+from gtts import gTTS
+from playsound import playsound
+import os, time, subprocess
+
+done = False
+print(time.perf_counter())
+r = sr.Recognizer()
+print(time.perf_counter())
+while done == False:
+    with sr.Microphone() as source:
+        print(time.perf_counter())
+        print("Recording")
+        # read the audio data from the default microphone
+        audio_data = r.record(source, duration=5)
+        print("Recognizing...")
+        # convert speech to text
+        try:
+            text = r.recognize_google(audio_data)
+            done = True
+        except:
+            pass
+        # text = " ".join(sys.argv[1:])
+        # print(text)
+
+#######
 
 predictor = Predictor()
 output = predictor.filterText(text)
@@ -13,11 +48,155 @@ with open("diagnosis.txt", "w") as f:
     f.write("\n")
 
 symptoms = [s[0] for s in output]
-output = predictor.processOutput(list(predictor.predict(symptoms).flatten()))
+avgseverity = sum([SEVERITY[i] for i in symptoms])/len(symptoms)
 
-result = "Possible Diagnoses:"
-for o in output:
-    result += " " + o[0] + " (" + str(int(100*o[1])) + "%) "
+if avgseverity > SEVERE_THRESHOLD:
+    if avgseverity > 8:
+        with open("diagnosis.txt", "a") as f:
+            f.write("These are extremely severe symptoms\n")
+    elif avgseverity > 5:
+        with open("diagnosis.txt", "a") as f:
+            f.write("These are severe symptoms\n")
+    else:
+        with open("diagnosis.txt", "a") as f:
+            f.write("These are moderate symptoms\n")
 
-with open("diagnosis.txt", "a") as f:
-    f.write(result)
+    output = predictor.processOutput(list(predictor.predict(symptoms).flatten()))
+
+    result = "Possible Diagnoses:"
+    for o in output:
+        result += " " + o[0] + " (" + str(int(100*o[1])) + "%) "
+
+    with open("diagnosis.txt", "a") as f:
+        f.write(result)
+
+    symptomDescription = pd.read_csv('Data/symptom_Description.csv')
+    symptomPrecaution = pd.read_csv('Data/symptom_precaution.csv')
+else:
+    print("These are mild symptoms, moving on without diagnosis", file = open("diagnosis.txt"))
+
+#########
+
+CHUNK = 1024
+
+PyAudio = pyaudio
+language = 'en'
+
+with open("diagnosis.txt", "r") as f:
+    inpText = f.read().replace('\n', ' ')
+    inputText = inpText
+    print("1")
+    speechObj = gTTS(text=inputText,lang=language,slow=False)
+    print("2")
+    speechObj.save("outputVoice.mp3")
+    print("3")
+    subprocess.call(['ffmpeg', '-y', '-i', 'outputVoice.mp3', 'outputVoice.wav'])
+    print("4")
+    playsound('outputVoice.wav')
+    print("5")
+    wavFile = wave.open("outputVoice.wav",'rb')
+    print("6")
+
+#p = pyaudio.PyAudio()
+
+print("7")
+#stream = p.open(format=p.get_format_from_width(wavFile.getsampwidth()),
+#                    channels=wavFile.getnchannels(),
+#                    rate=wavFile.getframerate(),
+#                    output=True)
+
+print("8")
+#data = wavFile.readframes(CHUNK)
+
+print("9")
+#while data != '':
+#        stream.write(data)
+#        data = wavFile.readframes(CHUNK)
+
+#stream.stop_stream()
+#stream.close()
+
+#p.terminate()
+
+##########
+
+
+
+
+
+with open("diagnosis.txt","w+") as f:
+    for o in output:
+        f.write("\n Would you like to hear a description of "+ o[0] + " and a reccomeneded precaution to take when dealing with it? Please just say yes, or no.")
+        # f.write("\n If you think you have " + o[0] + "then we reccomend that you take the following precaution \n")
+        # f.write((symptomPrecaution.loc[symptomPrecaution['Disease']==o[0]]).iat[0,1])
+        # f.write("\n What is " + o[0] + "exactly, you may ask. Well, I'd be glad to tell you! \n")
+        # f.write((symptomDescription.loc[symptomDescription['Disease']==o[0]]).iat[0,1])
+        inpText = f.read().replace('\n', ' ')
+
+
+
+        #########
+
+        CHUNK = 1024
+
+        PyAudio = pyaudio
+        language = 'en'
+
+
+        inputText = inpText
+        speechObj = gTTS(text=inputText,lang=language,slow=False)
+        speechObj.save("outputVoice.mp3")
+        subprocess.call(['ffmpeg', '-y', '-i', 'outputVoice.mp3', 'outputVoice.wav'])
+        playsound('outputVoice.wav')
+        wavFile = wave.open("outputVoice.wav",'rb')
+
+        p = pyaudio.PyAudio()
+
+        stream = p.open(format=p.get_format_from_width(wavFile.getsampwidth()),
+                            channels=wavFile.getnchannels(),
+                            rate=wavFile.getframerate(),
+                            output=True)
+
+        data = wavFile.readframes(CHUNK)
+
+        while data != '':
+                stream.write(data)
+                data = wavFile.readframes(CHUNK)
+
+        stream.stop_stream()
+        stream.close()
+
+        p.terminate()
+
+        done = False
+        print(time.perf_counter())
+        r = sr.Recognizer()
+        print(time.perf_counter())
+        while done == False:
+            with sr.Microphone() as source:
+                print(time.perf_counter())
+                print("Recording")
+                # read the audio data from the default microphone
+                audio_data = r.record(source, duration=5)
+                print("Recognizing...")
+                # convert speech to text
+                try:
+                    text = r.recognize_google(audio_data)
+                    done = True
+                except:
+                    pass
+        
+        if text.lower() == "yes":
+            f.truncate(0)
+            f.seek(0)
+            f.write("\n If you think you have " + o[0] + "then we reccomend that you take the following precaution \n")
+            f.write((symptomPrecaution.loc[symptomPrecaution['Disease']==o[0]]).iat[0,1])
+            f.write("\n What is " + o[0] + "exactly, you may ask. Well, I'd be glad to tell you! \n")
+            f.write((symptomDescription.loc[symptomDescription['Disease']==o[0]]).iat[0,1])
+        else:
+            f.truncate(0)
+            f.seek(0)
+
+        
+        
+
---
+++ app.py
@@ -0,0 +1,48 @@
+from tkinter import *
+from PIL import Image,ImageTk
+import copy
+import os
+
+c1,c2,c3,c4,c5 = '#2C4251','#D16666','#56CBF9','#A8C69F','#CCE2A3'
+
+width,height = 800,800
+def _create_circle(self,x,y,r,**kwargs):
+   return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
+Canvas.create_circle = _create_circle
+
+root=Tk()
+canvas = Canvas(root,width=width,height=height,bg=c4)
+canvas.pack()
+goose = Image.open("HealthHonkLogo.png")
+resize_geese = goose.resize((width//2,height//2))
+img = ImageTk.PhotoImage(resize_geese)
+#canvas.create_rectangle(0,0,width,height//2+height//8,fill=c4)
+canvas.create_image(width//2,height//8, anchor=N, image=img)
+#canvas.create_rectangle(0,height//2+height//8,height,width,fill="#2C4251")
+
+def goHome():
+   # Thing that happens if you press the home button#
+   pass
+def goAbout():
+   # Thing that happens if you press the about button#
+   pass
+def record():
+   os.system("python App/main.py")
+
+hImg = Image.open("HomeButton.png").resize((120,54))
+aImg = Image.open("AboutImage.png").resize((120,54))
+rImg = Image.open("recordButton.png").resize((175,175))
+tImg = ImageTk.PhotoImage(Image.open("HealthHonkTitle.png").resize((350,94)))
+homeImage = ImageTk.PhotoImage(hImg)
+aboutImage = ImageTk.PhotoImage(aImg)
+recordImage = ImageTk.PhotoImage(rImg)
+homeButton = Button(root,image = homeImage, command = goHome)
+aboutButton = Button(root,image = aboutImage, command = goAbout)
+recordButton = Button(root,image = recordImage, command = record, borderwidth=0, fg = 'green')
+#canvas.create_image(width//2,height*(9/11),anchor=S,image = recordImage)
+canvas.create_image(width//2,height*(1/12),anchor=N,image = tImg)
+homeButton.place(x=10,y=10)
+aboutButton.place(x=150,y=10)
+recordButton.place(x=width//2,y=height*(9/11),anchor=S)
+
+root.mainloop()
